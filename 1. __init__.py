3dccbd13717a3c501270e82ca5851dcf5978d155
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-01 22:38:06 +0100

    Update 1. __init__.py

--- 1. __init__.py
+++ 1. __init__.py
@@ -1,3 +1,4 @@
+etoirnounouveay repertoite sur mon ordi \Documents\GitHub\BitPredector
 Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> # app/__init__.py
